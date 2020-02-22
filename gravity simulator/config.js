95e81ec9c7c343d8e86c0fa8186c1e03d068055a
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-02-22 23:16:02 +0530

    initial commit

---
+++ gravity simulator/config.js
@@ -0,0 +1,20 @@
+const config = {
+    destroyFactor: 20,
+
+    spotDensity: 18750,
+    spotColor: "#000000",
+
+    objectColor: "#0091FF",
+    objectDensity: 4,
+    trailColor: "rgba(0, 145, 255, 0.2)",
+    trailWidth: 3,
+    trailLength: 75,
+
+    powerMult: 0.004,
+    maxDistance: 240,
+    arrowDotSpacing: 40,
+    arrowDotRadius: 8,
+    arrowDotColor: "#BBBBBBA5",
+
+    warpSpeed: 0
+}
